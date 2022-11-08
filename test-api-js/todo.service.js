ce337d7364cfaa62c27857d879824e603dcd2912
chandanaSLU <113061501+chandanaSLU@users.noreply.github.com>
2022-11-07 23:25:03 -0600

    Added testcases and functionality
    Added testcases and functionality

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -1,39 +1,50 @@
-class todoservice{
-    todo_data = {
-        "todo":[{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        }]
-    }
-    constructor(){
-        this.todos=this.todo_data;
-    }
+class todoservice {
+  todo_data = {
+    todo: [
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T2",
+        description: "D2",
+        done: true,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+    ],
+  };
+  constructor() {
+    this.todos = this.todo_data;
+  }
 
-    get_todos(){
+  get_todos() {
+    return this.todos;
+  }
+
+  add_todo(todo) {
+   let len=this.todo_data["todo"].length;
+	this.todo_data["todo][len]=todo;
+  }
+
+  delete_todo(id) {
+    this.todos.todo.pop(id)
         return this.todos;
-    }
+  }
 
-    add_todo(todo){
-        // Your code here
-    }
-
-    delete_todo(id){
-        // Your code here
-    }
-
-    update_todo(id, todo){
-        // Your code here
-    }
+  update_todo(id, todo) {
+    this.todos.todo[id] = todo;
+    return this.todos.todo[id];
+  }
 }
 
-
-module.exports= todoservice;
+module.exports = todoservice;
--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -23,5 +23,19 @@ describe('todo test suite', () => {
 
     // Write all your test cases here that corresponds to software requirements
 
+ test("add_todo", () => {
+        expect(todo_service.add_todo().todo.length).toEqual(3);
+    });
+test("delete_todos", () => {
+        expect(todo_service.delete_todo(id).todo.length).toEqual(1);
+    });
 
+describe("update testing", () => {
+  let todoService2 = new todoservice();
+  test("Update instance", () => {
+    expect(todoService2.update_todo(2, {
+        title : "Updated-T2",
+        description: "Updated-T3",
+        done: true
+    })).toBe(todoService2.get_todos().todo[2]);
 });
